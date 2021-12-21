4d59e1db3c0df09eff377da45a98805479c9b2c8
p43lz3r <16374477+paelzer@users.noreply.github.com>
2021-12-21 21:42:41 +0100

    Update ESP32_OBD_Engine_Oil_Temp.ino
    Removed useless bla bla

--- ESP32_OBD_Engine_Oil_Temp.ino
+++ ESP32_OBD_Engine_Oil_Temp.ino
@@ -2,7 +2,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-//OBD related
+//OBD connection related
 #include "BluetoothSerial.h"
 #include "ELMduino.h"
 
@@ -18,33 +18,16 @@ uint32_t oiltemp = 0;
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define SCREEN_ADDRESS 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-#define NUMFLAKES     10 // Number of snowflakes in the animation example
 
 #define LOGO_HEIGHT   16
 #define LOGO_WIDTH    16
-static const unsigned char PROGMEM logo_bmp[] =
-{ 0b00000000, 0b11000000,
-  0b00000001, 0b11000000,
-  0b00000001, 0b11000000,
-  0b00000011, 0b11100000,
-  0b11110011, 0b11100000,
-  0b11111110, 0b11111000,
-  0b01111110, 0b11111111,
-  0b00110011, 0b10011111,
-  0b00011111, 0b11111100,
-  0b00001101, 0b01110000,
-  0b00011011, 0b10100000,
-  0b00111111, 0b11100000,
-  0b00111111, 0b11110000,
-  0b01111100, 0b11110000,
-  0b01110000, 0b01110000,
-  0b00000000, 0b00110000
-};
+
 
 void setup() {
+
+  // Initialize serial and Bluetooth connection
   Serial.begin(115200);
   SerialBT.setPin("1234");
   ELM_PORT.begin("ArduHUD", true);
